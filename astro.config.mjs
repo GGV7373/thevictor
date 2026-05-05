415f5f5d3594a15d593ec198e11209fa805dc8e3
Victor <GGVictor7373@outlook.com>
2026-05-05 09:35:10 +0200

    refactor: remove Cloudflare adapter and related scripts from configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,4 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
-// https://astro.build/config
-export default defineConfig({
-  adapter: cloudflare({
-    prerenderEnvironment: 'node',
-    // @ts-ignore - platformProxy is a valid option for cloudflare adapter
-    platformProxy: {
-      enabled: true
-    },
-    imageService: "cloudflare"
-  })
-});
+export default defineConfig({});
